28e36276bb8a37c1d02b074cd70b344376f576e8
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2022-10-27 21:10:23 -0400

    Correction du bug dans deplacement carre bleu + propreté du code

--- controleur.py
+++ controleur.py
@@ -2,44 +2,38 @@ from vue import JeuVue, MenuVue
 from modeles import BordureNoire, ZoneBlanche, CarreRouge, RectangleBleu
 import csv
 import time
-import random
 from c31Geometry2 import *
-from functools import partial
 
 class MenuControleur:
-    def __init__(self, root, jeuControleur) :
+    def __init__(self, root, jeuControleur):
         self.jeuControleur = jeuControleur
-        self.vue = MenuVue(root, self.nouvellePartie, self.lireScore ,self.quitter)
-        #nom = self.vue.demanderNom(root)
-        #self.vue.setNom(nom)
+        self.vue = MenuVue(root, self.nouvellePartie, self.lireScore, self.quitter)
 
     def debuter(self) :
         self.vue.draw()                        
     
-    def nouvellePartie(self) :
-        if self.jeuControleur.demarrerPartie() :
+    def nouvellePartie(self):
+        if self.jeuControleur.demarrerPartie():
             root = self.jeuControleur.vue.root
             self.jeuControleur.vue.destroy()  
             self.jeuControleur = JeuControleur(root)
         self.jeuControleur.debuter()
 
-    def quitter(self) :
-       self.jeuControleur.vue.destroy()
+    def quitter(self):
+        self.jeuControleur.vue.destroy()
 
-    def lireScore(self) :
-        with open('FichierScores.csv', 'r') as csvFile :
+    def lireScore(self):
+        with open('FichierScores.csv', 'r') as csvFile:
             lecteur_score = csv.reader(csvFile, delimiter=',')
             self.dataList = [[]]
             self.dataRead = []
             cpt = 0
-
             for row in lecteur_score :
                 self.dataRead.append(row)
                 cpt += 1
                 if cpt % 4 == 0 :
                     self.dataList.append(self.dataRead)
                     self.dataRead = []
-
 
 class JeuControleur :
     def __init__(self, root) :
@@ -55,51 +49,51 @@ class JeuControleur :
         self.vue.setDif(self.difficulte)
         self.rectangleBleu = []
         self.itemCollection = []
-        for i in range(0, 4) :
+        for i in range(0, 4):
             self.rectangleBleu.append(RectangleBleu(1,i+1, self.vue.canvas))
         self.debuter()
         self.__defineEvent()
         
-    def demarrerPartie(self) :
+    def demarrerPartie(self):
         return self.partieDemarree
-    
 
-    def __defineEvent(self) :
+    def __defineEvent(self):
         self.vue.setListen("<ButtonPress-1>", self.evenement)
 
-    def evenement(self, event) :
-        self.vue.setListen("<Motion>", self.evenement)
+    def evenement(self, event):
         tempsDebut = time.time()
-        
-        x = event.x
-        y = event.y
-        self.deplacementRectangleBleu()
-        self.deplacementCarreRouge(x, y)
-        tempsFin = time.time()
-        self.minuteur(tempsFin - tempsDebut)
-        temps = tempsFin - tempsDebut
-        self.ecrireScore("{:.2f}".format(temps))
-        self.vue.setTimer("{:.2f}".format(temps))  # Pour afficher 2 chiffres après la virgule
+        self.vue.setListen("<Motion>", self.evenement)
+        self.roulerJeu(event.x, event.y)
+        if self.verifierCollision():
+            tempsFin = time.time()
+            self.minuteur(tempsFin - tempsDebut)
+            temps = tempsFin - tempsDebut
+            self.ecrireScore("{:.2f}".format(temps))
+            self.vue.setTimer("{:.2f}".format(temps))  # Pour afficher 2 chiffres après la virgule
 
     def debuter(self) :
         self.partieDemarree = True
         self.vue.draw(self.rectangleBleu)
         self.vue.drawCarre(self.carreRouge)
 
-    
+    def roulerJeu(self, x, y):
+        self.deplacementRectangleBleu()
+        self.deplacementCarreRouge(x, y)
+        self.verifierCollision()
+
     def verifierCollision(self) :
         # On recupere la position du carré rouge
         carreX = self.carreRouge.get_origine().x
         carreY = self.carreRouge.get_origine().y
 
         # Détéction de collision avec la bordure noire
-        if carreX >= 75 and carreX <= 85 :
+        if carreX >= 75 and carreX <= 85:
             return True
-        elif carreX >= 465 and carreX <= 490 :
+        elif carreX >= 465 and carreX <= 490:
             return True
-        elif carreY >= 88 and carreY <= 96 : 
+        elif carreY >= 88 and carreY <= 96:
             return True
-        elif carreY >= 465 and carreY <= 475 :
+        elif carreY >= 465 and carreY <= 475:
             return True
         else :
             for i in range(0, 4) :
@@ -126,7 +120,6 @@ class JeuControleur :
         with open('FichierScores.csv', 'a') as csvFile :
             ecriture_score = csv.writer(csvFile, delimiter=',')
             ecriture_score.writerow(self.fileData)
-    
 
     '''
     def trier(self) :                                       ###### pas sur du fonctionnement de cette fonction! demander a isi
@@ -156,46 +149,45 @@ class JeuControleur :
             
             # DÉTECTION DE COLLISIONS LATÉRALES 
             if x >= 75 and x <= 85 : #collision bordure gauche (axes de directions ouest deviennent de direction est)
-                if self.rectangleBleu[i].getAxe() == 0 :
+                if self.rectangleBleu[i].getAxe() == 0:
                     self.rectangleBleu[i].setAxe(1)
                 else : 
                     self.rectangleBleu[i].setAxe(2)
             if x >= 465 and x <= 490 : #collision bordure droite (axes de directions est deviennent de direction ouest)
-                if self.rectangleBleu[i].getAxe() == 1 :
+                if self.rectangleBleu[i].getAxe() == 1:
                     self.rectangleBleu[i].setAxe(0)
                 else :
                     self.rectangleBleu[i].setAxe(3)
-            if y >= 88 and y <= 96 : #collision bordure haut (axes de directions nord deviennent de direction sud)
-                if self.rectangleBleu[i].getAxe() == 1 :
+            if y >= 70 and y <= 80 : #collision bordure haut (axes de directions nord deviennent de direction sud)
+                if self.rectangleBleu[i].getAxe() == 1:
                     self.rectangleBleu[i].setAxe(2)
                 else :
                     self.rectangleBleu[i].setAxe(3)
             if y >= 465 and y <= 475 : #collision bordure bas (axes de directions sud deviennent de direction nord)
-                if self.rectangleBleu[i].getAxe() == 2 :
+                if self.rectangleBleu[i].getAxe() == 2:
                     self.rectangleBleu[i].setAxe(1)
                 else :
                     self.rectangleBleu[i].setAxe(0)
 
             # DÉPLACEMENT LOGIQUE
-            if self.rectangleBleu[i].getAxe() == 0 :
-                    x -= 0.2
-                    y -= 0.2
-            elif self.rectangleBleu[i].getAxe() == 1 :
-                    x += 0.2
-                    y -= 0.2
+            if self.rectangleBleu[i].getAxe() == 0:
+                x -= 0.2
+                y -= 0.2
+            elif self.rectangleBleu[i].getAxe() == 1:
+                x += 0.2
+                y -= 0.2
             elif self.rectangleBleu[i].getAxe() == 2 :
-                    x += 0.2
-                    y += 0.2
+                x += 0.2
+                y += 0.2
             elif self.rectangleBleu[i].getAxe() == 3 :
-                    x -= 0.2
-                    y += 0.2
+                x -= 0.2
+                y += 0.2
 
             # AFFECTATIONS MODÈLES & VUE
             deplacement = Vecteur(x, y)
             self.rectangleBleu[i].translateTo(deplacement)
             self.rectangleBleu[i].modificationPos(deplacement)
         self.vue.draw(self.rectangleBleu)
-                                                                             
                                             
     def deplacementCarreRouge(self, x, y) : 
         deplacement = Vecteur(x, y) 

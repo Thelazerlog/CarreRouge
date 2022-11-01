3e5a217d9609c69822e4569620b5f177a97e2dcb
Kerian Devillers <98911750+Thelazerlog@users.noreply.github.com>
2022-11-01 11:26:48 -0400

    vertices

--- modeles.py
+++ modeles.py
@@ -24,14 +24,14 @@ class CarreRouge(Carre):
         """
         self.axeDeplacement = random.randint(0, 3)
         self.arrete = 40
-        self.vertices = [Vecteur(0,0), Vecteur(0,0), Vecteur(0,0), Vecteur(0,0)]
         super().__init__(canvas, Vecteur(225, 225), self.arrete, 0 , "red", "red", 0)
-        #self.vertices.append = Vecteur(0,0)
-        #self.vertices.append = Vecteur(0,0)
-        #self.vertices.append = Vecteur(0,0)
-        #self.vertices.append = Vecteur(0,0)
+        #self.vertices = [Vecteur(self.origine.x - self.arrete / 2, self.origine.y - self.arrete / 2), Vecteur(self.origine.x + self.arrete / 2, self.origine.y - self.arrete / 2), Vecteur(self.origine.x + self.arrete / 2, self.origine.y + self.arrete / 2), Vecteur(self.origine.x - self.arrete / 2,self.origine.y + self.arrete / 2)]
 
-
+    #def resetVertices(self):
+        #self.vertices[0] = Vecteur(self.origine.x - self.arrete / 2, self.origine.y - self.arrete / 2)
+        #self.vertices[1] = Vecteur(self.origine.x + self.arrete / 2, self.origine.y - self.arrete / 2)
+        #self.vertices[2] = Vecteur(self.origine.x + self.arrete / 2, self.origine.y + self.arrete / 2)
+        #self.vertices[3] = Vecteur(self.origine.x - self.arrete / 2, self.origine.y + self.arrete / 2)
     def getOrigine(self) -> Vecteur:
         """Permet de récupérer l'origine du carré
 
